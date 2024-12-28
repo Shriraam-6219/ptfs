b882108e2c643d709f01287637030de91566d8c7
Shriraam MKS <125351871+Shriraam-6219@users.noreply.github.com>
2024-12-28 11:09:04 +0530

    Control statements added
    1) Picture moved to the assets folder.
    2) New folder created to practice JavaScript.

---
+++ JavaScript/secondaryDatatypes.js
@@ -0,0 +1,46 @@
+//Array Examples
+
+// array = [10,20,30,[40,5,50,6],"a","KEC",true]
+// console.log(array);
+
+//Object Example type 1:
+
+// obj ={
+//     "firstName":"Jai",
+//     "lastName":"Kumar",
+//     "age":20
+// }
+
+// console.log(obj);
+
+//Object Example type 2
+
+// objType2 ={};
+// console.log(objType2);
+
+//Object Example type 3
+
+// objType3 = new Object();
+// console.log(objType3);
+
+//Object Example type 2
+
+// objType2 = {};
+// objType2 ["movie"] = "Iron Man 2";
+// objType2 ["budget"] = "100cr";
+// objType2 ["boxOffice"] = "1000cr";
+// objType2 ["release"] = "Worldwide";
+
+
+// console.log(objType2);
+// console.log(objType2["boxOffice"]);
+// console.log(objType2.budget);
+
+//Object Example type 3:
+
+// objType5 = new Object();
+// objType5["phone"] = "iPhone";
+// objType5["store"] = "App Store";
+// objType5["game1"] = "Sudoku";
+// objType5["game2"] = "Chess";
+// console.log(objType5);
